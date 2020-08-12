79e849b21b8f868ae0197d779b1464695a81141f
BakedPVP <gameking36@msn.com>
2020-08-12 02:40:06 -0400

    Add JSDoc comments to Discord:User:Action module

--- src/.vizality/modules/discord/user/action/clearNote.js
+++ src/.vizality/modules/discord/user/action/clearNote.js
@@ -2,10 +2,17 @@ const { logger: { error } } = require('@utilities');
 
 const setNote = require('./setNote');
 
+/**
+ * Clears a note's contents for the user.
+ *
+ * @param {string} userId - User ID
+ * @returns {undefined} Action
+ */
 const clearNote = (userId) => {
   const _module = 'Module';
   const _submodule = 'Discord:User:Action:clearUserNote';
 
+  // Check if the user ID is a valid string
   if (typeof userId !== 'string') {
     return error(_module, _submodule, null, `User ID must be a valid string.`);
   }
--- src/.vizality/modules/discord/user/action/setNote.js
+++ src/.vizality/modules/discord/user/action/setNote.js
@@ -1,14 +1,23 @@
 const { logger: { error } } = require('@utilities');
 const { getModule } = require('@webpack');
 
+/**
+ * Sets a note's contents for the user.
+ *
+ * @param {string} userId - User ID
+ * @param {string} note - User note
+ * @returns {undefined} Action
+ */
 const setNote = (userId, note) => {
   const _module = 'Module';
   const _submodule = 'Discord:User:Action:setUserNote';
 
+  // Check if the user ID is a valid string
   if (typeof userId !== 'string') {
     return error(_module, _submodule, null, `User ID must be a valid string.`);
   }
 
+  // Check if the user note is a valid string
   if (typeof note !== 'string') {
     return error(_module, _submodule, null, `Note must be a valid string.`);
   }
