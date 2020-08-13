73492be5482a4d401d2f023a6510333ad3f1927b
BakedPVP <gameking36@msn.com>
2020-08-13 18:08:08 -0400

    Rename Discord:Modules module to Discord:Module

--- src/.vizality/modules/discord/module/index.js
+++ src/.vizality/modules/discord/module/index.js
@@ -1,3 +1,10 @@
+/**
+ * Module module.
+ * Contains useful webpack module collections,
+ * usually in the form of data objects.
+ *
+ * @module discord.module
+ */
 require('fs')
   .readdirSync(__dirname)
   .filter(file => file !== 'index.js')
