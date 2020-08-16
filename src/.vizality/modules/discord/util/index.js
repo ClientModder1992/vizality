2a50ab9f306caaae7a7ba01f75c329f0afc9df70
BakedPVP <gameking36@msn.com>
2020-08-16 19:35:36 -0400

    Update Discord Util module
    - Change from utility to util
    - Remove getCreationDate
    - Remove isValidId
    - Remove getValidId

---
+++ src/.vizality/modules/discord/util/index.js
@@ -0,0 +1,12 @@
+/**
+ * Utility module.
+ * Contains utility functions that may be used by the other modules.
+ * @namespace discord.util
+ * @module discord.util
+ * @memberof discord
+ */
+const util = {
+
+};
+
+module.exports = util;
