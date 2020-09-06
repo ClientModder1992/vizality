920b9a6860aa0bd0629d68de27c344e3625550f7
BakedPVP <gameking36@msn.com>
2020-09-06 13:12:24 -0400

    Update eslintrc, fix up codeblocks plugin, shift codeblock CSS towards BEM style, start adding codeblock theme styles

--- addons/plugins/vz-codeblocks/index.js
+++ addons/plugins/vz-codeblocks/index.js
@@ -1,12 +1,13 @@
 const { clipboard } = require('electron');
 
-const { react: { findInReactTree } } = require('@util');
+const { joinClassNames, react: { findInReactTree, getReactInstance } } = require('@util');
 const { getModule, hljs } = require('@webpack');
 const { patch, unpatch } = require('@patcher');
 const { Plugin } = require('@entities');
+const { Messages } = require('@i18n');
 const { React } = require('@react');
 
-module.exports = class Codeblocks extends Plugin {
+module.exports = class CodeBlocks extends Plugin {
   onStart () {
     this.injectStyles('styles/main.scss');
     this.patchCodeblocks();
@@ -15,15 +16,13 @@ module.exports = class Codeblocks extends Plugin {
   onStop () {
     unpatch('vz-codeblocks-inline');
     unpatch('vz-codeblocks-embed');
-
-    document.querySelectorAll('.hljs [class^=vizality]').forEach(e => e.style.display = 'none');
+    this._forceUpdate();
   }
 
   async patchCodeblocks () {
     const parser = getModule('parse', 'parseTopic');
     patch('vz-codeblocks-inline', parser.defaultRules.codeBlock, 'react', (args, res) => {
       this.injectCodeblock(args, res);
-
       return res;
     });
 
@@ -34,9 +33,9 @@ module.exports = class Codeblocks extends Plugin {
           this.injectCodeblock(null, codeblock);
         }
       }
-
       return res;
     });
+    this._forceUpdate();
   }
 
   injectCodeblock (args, codeblock) {
@@ -44,37 +43,53 @@ module.exports = class Codeblocks extends Plugin {
 
     codeblock.props.render = (codeblock) => {
       const res = render(codeblock);
-
       const { children } = res.props;
+      const isDangerouslySetInnerHTML = children.props.dangerouslySetInnerHTML;
       const lang = args ? args[0].lang : children.props.className.split(' ').find(className => !className.includes('-') && className !== 'hljs');
 
-      if (children.props.dangerouslySetInnerHTML) {
-        children.props.children = this.renderCodeblock(lang, children.props.dangerouslySetInnerHTML);
+      // @todo Don't forget to make these into settings and add `theme`.
+      children.props.className = joinClassNames(
+        children.props.className,
+        'vz-code-block',
+        'vz-has-line-numbers',
+        'vz-has-copy-button',
+        {
+          'vz-has-header': isDangerouslySetInnerHTML
+        }
+      );
 
+      if (children.props.dangerouslySetInnerHTML) {
+        res.props.className = joinClassNames(res.props.className, 'vz-code-block__pre');
+        children.props.children = this.renderCodeblock(lang, children.props.dangerouslySetInnerHTML);
         delete children.props.dangerouslySetInnerHTML;
       } else if (typeof children.props.children === 'string') {
         children.props.children = this.renderCodeblock(lang, children.props.children);
       }
-
       return res;
     };
+  }
+
+  _forceUpdate () {
+    document.querySelectorAll(`[id^='chat-messages-']`).forEach(e => getReactInstance(e).memoizedProps.onMouseMove());
   }
 
   renderCodeblock (lang, content) {
     const children = [];
     const isDangerouslySetInnerHTML = typeof content === 'object';
-    const isValidLanguage = typeof hljs.getLanguage(lang) !== 'undefined';
+    const language = hljs.getLanguage(lang) ? hljs.getLanguage(lang).name : undefined;
+    const isValidLanguage = typeof language !== 'undefined';
 
     children.push(React.createElement('div', {
+      className: 'vz-code-block__inner',
       dangerouslySetInnerHTML: isDangerouslySetInnerHTML ? content : null
     }, isDangerouslySetInnerHTML ? null : content), isValidLanguage && React.createElement('div', {
-      className: 'vizality-codeblock-lang'
-    }, lang), React.createElement('div', {
-      className: 'vizality-lines'
+      className: 'vz-code-block__header'
+    }, language), React.createElement('div', {
+      className: 'vz-code-block__line-numbers'
     }), React.createElement('button', {
-      className: 'vizality-codeblock-copy-btn',
+      className: 'vz-code-block__copy-button',
       onClick: this._onClickHandler
-    }, 'copy'));
+    }, Messages.COPY));
 
     return children;
   }
@@ -82,23 +97,17 @@ module.exports = class Codeblocks extends Plugin {
   _onClickHandler (e) {
     const { target } = e;
 
-    if (target.classList.contains('copied')) {
-      return;
-    }
+    if (target.classList.contains('vz-is-copied')) return;
 
-    target.innerText = 'copied!';
-    target.classList.add('copied');
+    target.innerText = Messages.COPIED;
+    target.classList.add('vz-is-copied');
 
     setTimeout(() => {
-      target.innerText = 'copy';
-      target.classList.remove('copied');
+      target.innerText = Messages.COPY;
+      target.classList.remove('vz-is-copied');
     }, 1000);
 
     const codeContent = target.parentElement.children[0];
-    const vzCopy = codeContent.querySelector('[data-vizality-codeblock-copy]');
-    if (vzCopy) {
-      return clipboard.writeText(vzCopy.textContent);
-    }
 
     const range = document.createRange();
     range.selectNode(codeContent);
@@ -107,7 +116,7 @@ module.exports = class Codeblocks extends Plugin {
     selection.removeAllRanges();
     selection.addRange(range);
 
-    clipboard.writeText(selection.toString());
+    clipboard.writeText(selection.toString().trim());
 
     selection.removeAllRanges();
   }
