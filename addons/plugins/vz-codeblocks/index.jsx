a28a1c16115638f9ab80936708d2a96a1c906ff5
BakedPVP <gameking36@msn.com>
2020-09-08 22:22:01 -0400

    Refactor code block injection code

---
+++ addons/plugins/vz-codeblocks/index.jsx
@@ -0,0 +1,67 @@
+const { joinClassNames, react: { findInReactTree, getReactInstance } } = require('@util');
+const { getModule } = require('@webpack');
+const { patch, unpatch } = require('@patcher');
+const { CodeBlock } = require('@components');
+const { Plugin } = require('@entities');
+const { React } = require('@react');
+
+module.exports = class CodeBlocks extends Plugin {
+  onStart () {
+    this.injectStyles('styles/main.scss');
+    this.patchCodeblocks();
+  }
+
+  onStop () {
+    unpatch('vz-codeblocks-inline');
+    unpatch('vz-codeblocks-embed');
+    this._forceUpdate();
+  }
+
+  async patchCodeblocks () {
+    const parser = getModule('parse', 'parseTopic');
+    patch('vz-codeblocks-inline', parser.defaultRules.codeBlock, 'react', (args, res) => {
+      this.injectCodeblock(args, res);
+      return res;
+    });
+
+    patch('vz-codeblocks-embed', parser, 'parseAllowLinks', (args, res) => {
+      for (const children of res) {
+        const codeblock = findInReactTree(children, n => n.type && n.type.name === '');
+        if (codeblock) {
+          this.injectCodeblock(null, codeblock);
+        }
+      }
+      return res;
+    });
+    this._forceUpdate();
+  }
+
+  injectCodeblock (args, codeblock) {
+    const { render } = codeblock.props;
+
+    codeblock.props.render = (codeblock) => {
+      let lang, content, contentIsRaw, res;
+      if (!args) {
+        res = render(codeblock);
+        lang = res?.props?.children?.props?.className.split(' ').find(className => !className.includes('-') && className !== 'hljs');
+        if (res?.props?.children?.props?.children) {
+          content = res?.props?.children?.props?.children;
+        } else {
+          content = res?.props?.children?.props?.dangerouslySetInnerHTML?.__html;
+          contentIsRaw = true;
+        }
+      } else {
+        lang = args[0]?.lang;
+        content = args[0]?.content;
+      }
+
+      res = <CodeBlock language={lang} content={content} contentIsRaw={contentIsRaw} hasWrapper={false} />;
+
+      return res;
+    };
+  }
+
+  _forceUpdate () {
+    document.querySelectorAll(`[id^='chat-messages-']`).forEach(e => getReactInstance(e).memoizedProps.onMouseMove());
+  }
+};
