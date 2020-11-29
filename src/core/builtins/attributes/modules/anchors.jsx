fc253865a4bdb32dd5620ed7e804d45c16f09a6c
dperolio <dustin.perolio@gmail.com>
2020-11-29 10:08:34 -0500

    Remove Youtube anchor shenanigans

--- src/core/builtins/attributes/modules/anchors.jsx
+++ src/core/builtins/attributes/modules/anchors.jsx
@@ -1,16 +1,5 @@
 const { patch, unpatch } = require('@vizality/patcher');
 const { getModule } = require('@vizality/webpack');
-const { React } = require('@vizality/react');
-
-const youtubeRegex = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/i;
-
-const getYoutubeVideoId = url => {
-  const match = url.match(youtubeRegex);
-
-  return (match && match[2].length === 11)
-    ? match[2]
-    : null;
-};
 
 module.exports = async () => {
   const Anchor = getModule(m => m.default && m.default.displayName === 'Anchor');
@@ -38,24 +27,6 @@ module.exports = async () => {
             content: `We were unable to locate that user.`,
             type: 'error'
           }));
-      };
-    }
-
-    if (new RegExp(youtubeRegex).test(res.props.href)) {
-      res.props.onClick = e => {
-        e.preventDefault();
-        const id = getYoutubeVideoId(res.props.href);
-        vizality.api.popups.openWindow({ alwaysOnTop: true, render: props =>
-          <iframe
-            width='100%'
-            height='100%'
-            src={`https://www.youtube.com/embed/${id}?autoplay=1`}
-            frameBorder='0'
-            allowFullScreen='allowfullscreen'
-            allow='autoplay'
-            {...props}
-          />
-        }, { width: 494, height: 300 });
       };
     }
 
