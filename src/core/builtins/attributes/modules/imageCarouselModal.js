7ddd1e371aed6ef4aef7ce40ef3cbc70bea1be7c
dperolio <dustin.perolio@gmail.com>
2020-12-01 06:46:35 -0500

    Add a patch for the image carousel modal

---
+++ src/core/builtins/attributes/modules/imageCarouselModal.js
@@ -0,0 +1,26 @@
+const { getModuleByDisplayName } = require('@vizality/webpack');
+const { react: { findInTree } } = require('@vizality/util');
+const { patch, unpatch } = require('@vizality/patcher');
+
+module.exports = async () => {
+  const ModalCarousel = getModuleByDisplayName('componentDispatchSubscriber(ModalCarousel)');
+
+  patch('vz-attributes-modal-image-carousel', ModalCarousel.prototype, 'render', (_, res) => {
+    const ogRef = res.ref;
+
+    res.ref = elem => {
+      const r = ogRef(elem);
+      if (elem && elem._reactInternalFiber) {
+        const container = findInTree(elem._reactInternalFiber.return, el => el.stateNode && el.stateNode.setAttribute, { walkable: [ 'return' ] });
+        container.stateNode.parentElement.setAttribute('vz-modal', 'image-carousel');
+        container.stateNode.children[0].style.width = `${res.props.items[res.props.startWith].width}px`;
+      }
+      return r;
+    };
+
+    return res;
+  });
+
+
+  return () => unpatch('vz-attributes-modal-image-carousel');
+};
