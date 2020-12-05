82367383efb263f755c2fea88e7ef61e92f870dc
dperolio <dustin.perolio@gmail.com>
2020-12-04 21:45:42 -0500

    Add some logs for debugging

--- src/inject/win32.js
+++ src/inject/win32.js
@@ -5,9 +5,11 @@ exports.getAppDir = async () => {
   const discordPath = join(process.env.LOCALAPPDATA, 'Discord');
   const discordDirectory = await readdir(discordPath);
 
+  console.log('discordPath', discordPath);
   const currentBuild = discordDirectory
     .filter(path => path.startsWith('app-'))
     .reverse()[0];
 
+  console.log('final', join(discordPath, currentBuild, 'resources', 'app'));
   return join(discordPath, currentBuild, 'resources', 'app');
 };
--- src/update/index.js
+++ src/update/index.js
@@ -5,9 +5,11 @@ const { inject } = require('../inject/main');
 const applicableEnvs = [ 'win32', 'darwin' ];
 
 if (applicableEnvs.includes(process.platform)) {
-  console.log('[Vizality] Detected an installation sensitive to host updates. Injecting into the updater');
+  console.log('[Vizality] Detected an installation sensitive to host updates. Injecting into the updater.');
   const injector = require(`../inject/${process.platform}`);
+  console.log('injector', injector);
   let squirrelUpdateScript = join(require.main.filename, '..', 'squirrelUpdate.js');
+  console.log('squirrelUpdateScript', squirrelUpdateScript);
   let squirrelRestart;
   try {
     squirrelRestart = require(squirrelUpdateScript).restart;
@@ -15,7 +17,7 @@ if (applicableEnvs.includes(process.platform)) {
     squirrelUpdateScript = join(require.main.filename, '../../app.asar', 'app_bootstrap/squirrelUpdate.js');
     squirrelRestart = require(squirrelUpdateScript).restart;
   }
-  require.cache[squirrelUpdateScript].exports.restart = function (app, newVersion) {
+  require.cache[squirrelUpdateScript].exports.restart = (app, newVersion) => {
     console.log('[Vizality] Injecting in the new version');
     inject(injector).then(() => squirrelRestart(app, newVersion));
   };
