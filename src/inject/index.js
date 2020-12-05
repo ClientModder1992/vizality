b303d4e453bb6d2be165066c748e88d0fd04e5ff
dperolio <dustin.perolio@gmail.com>
2020-12-04 20:21:45 -0500

    Wait I'm lost, I thought I removed those not added...

--- src/inject/index.js
+++ src/inject/index.js
@@ -4,11 +4,11 @@ require('./env_check')();
 
 const { promises: { writeFile } } = require('fs');
 const { join } = require('path');
-const main = require('./main.js');
+const main = require('./main');
 
 let platformModule;
 try {
-  platformModule = require(`./${process.platform}.js`);
+  platformModule = require(`./${process.platform}`);
 } catch (err) {
   if (err.code === 'MODULE_NOT_FOUND') {
     console.log(`\x1b[1m\x1b[31mERROR: \x1b[0mUnsupported platform '${process.platform}'`);
