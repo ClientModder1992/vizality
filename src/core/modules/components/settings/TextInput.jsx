596c64f8d8459d332f5c03a0dab2b381a9e2efbc
dperolio <dustin.perolio@gmail.com>
2020-12-05 21:12:36 -0500

    Fix TextInput titles being eaten

--- src/core/modules/components/settings/TextInput.jsx
+++ src/core/modules/components/settings/TextInput.jsx
@@ -6,20 +6,8 @@ const FormItem = require('./FormItem');
 
 const Input = AsyncComponent.from(getModuleByDisplayName('TextInput', true));
 
-// module.exports = class TextInput extends React.PureComponent {
-//   render () {
-//     const { children: title, note, required } = this.props;
-//     delete this.props.children;
-
-//     return (
-//       <FormItem title={title} note={note} required={required} noteHasMargin>
-//         <Input {...this.props} />
-//       </FormItem>
-//     );
-//   }
-// };
-
-module.exports = React.memo(({ title, note, required, ...props }) => {
+module.exports = React.memo(props => {
+  const { children: title, note, required } = props;
   delete props.children;
 
   return (
