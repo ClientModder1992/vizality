b1eea35e7e7b4dab749812e3a1cdd6caf846dfe7
dperolio <dustin.perolio@gmail.com>
2020-12-08 11:58:14 -0500

    Add keybinds to access Dashboard (alt+v) and leave dashboard (esc)

--- src/core/builtins/dashboard/index.jsx
+++ src/core/builtins/dashboard/index.jsx
@@ -1,3 +1,6 @@
+const { ipcRenderer } = require('electron');
+
+const { Regexes } = require('@vizality/constants');
 const { Builtin } = require('@vizality/entities');
 
 const Sidebar = require('./components/parts/sidebar/Sidebar');
@@ -12,9 +15,34 @@ module.exports = class Dashboard extends Builtin {
       render: Routes,
       sidebar: Sidebar
     });
+
+    vizality.api.keybinds.registerKeybind({
+      id: 'exit-dashboard',
+      executor: this.leaveDashboard,
+      shortcut: 'esc'
+    });
+
+    vizality.api.keybinds.registerKeybind({
+      id: 'go-to-dashboard',
+      executor: () => vizality.api.router.navigate('dashboard'),
+      shortcut: 'alt+v'
+    });
   }
 
   onStop () {
     vizality.api.router.unregisterRoute('/dashboard');
+    vizality.api.keybinds.unregisterKeybind('exit-dashboard');
+    vizality.api.keybinds.unregisterKeybind('go-to-dashboard');
+  }
+
+  async leaveDashboard () {
+    if (window.location.pathname.startsWith('/vizality/dashboard')) {
+      let history = await vizality.native.app.getHistory();
+      history = history.reverse();
+      history.shift();
+      const match = history.find(location => !location.includes('/vizality/dashboard'));
+      const route = match.replace(new RegExp(Regexes.DISCORD), '');
+      vizality.api.router.navigate(route);
+    }
   }
 };
