d436cff32a88945dfef1495d2621d33644c0e979
dperolio <dustin.perolio@gmail.com>
2020-12-10 13:38:16 -0500

    Fix anchors again, add channel member attribute

--- src/core/builtins/attributes/modules/channelMembers.js
+++ src/core/builtins/attributes/modules/channelMembers.js
@@ -7,8 +7,10 @@ module.exports = () => {
   patch('vz-attributes-channel-members-member', MemberListItem.prototype, 'render', function (_, res) {
     if (!this.props?.user) return res;
     const { user } = this.props;
+    const { activities } = res.props?.subText?.props;
 
     res.props['vz-user-id'] = user.id;
+    res.props['vz-activities'] = Boolean(activities.some(activity => activity.type !== 4)) && '';
     res.props['vz-self'] = Boolean(user.email) && '';
     res.props['vz-bot'] = Boolean(user.bot) && '';
     res.props['vz-owner'] = Boolean(this.props?.isOwner) && '';
