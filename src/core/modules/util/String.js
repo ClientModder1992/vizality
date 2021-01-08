92c354c42da7c66a5718d9e03a27c631d01953a8
dperolio <dustin.perolio@gmail.com>
2021-01-08 18:47:29 -0500

    Add util.string.owoify function (oh boy)

--- src/core/modules/util/String.js
+++ src/core/modules/util/String.js
@@ -189,3 +189,121 @@ export const assertString = input => {
     return new TypeError(`Input must be a string, but received type of ${typeof input}.`);
   }
 };
+
+/**
+ * OwO'ify string input.
+ * Sourced from @see {@link https://gist.github.com/aqua-lzma/ced43969ef48056791179138589ebcac}
+ * @param {*} string String to convert
+ */
+export const owoify = string => {
+  const stutterChance = 0.1;
+  const prefixChance = 0.05;
+  const suffixChance = 0.15;
+
+  const words = {
+    love: 'wuv',
+    mr: 'mistuh',
+    dog: 'doggo',
+    cat: 'kitteh',
+    hello: 'henwo',
+    hell: 'heck',
+    fuck: 'fwick',
+    fuk: 'fwick',
+    shit: 'shoot',
+    friend: 'fwend',
+    stop: 'stawp',
+    god: 'gosh',
+    dick: 'peepee',
+    penis: 'peepee'
+  };
+
+  const suffixes = [
+    '(ﾉ´ з `)ノ',
+    '( ´ ▽ ` ).｡ｏ♡',
+    '(´,,•ω•,,)♡',
+    '(*≧▽≦)',
+    'ɾ⚈▿⚈ɹ',
+    '( ﾟ∀ ﾟ)',
+    '( ・ ̫・)',
+    '( •́ .̫ •̀ )',
+    '(▰˘v˘▰)',
+    '(・ω・)',
+    '✾(〜 ☌ω☌)〜✾',
+    '(ᗒᗨᗕ)',
+    '(・`ω´・)',
+    ':3',
+    '>:3',
+    'hehe',
+    'xox',
+    '>3<',
+    'murr~',
+    'UwU',
+    '*gwomps*'
+  ];
+
+  const prefixes = [
+    'OwO',
+    'OwO whats this?',
+    '*unbuttons shirt*',
+    '*nuzzles*',
+    '*waises paw*',
+    '*notices bulge*',
+    '*blushes*',
+    '*giggles*',
+    'hehe'
+  ];
+
+  function replaceAll (string, map) {
+    const source = Object.keys(map).map(i => `\\b${i}`);
+    const re = new RegExp(`(?:${source.join(')|(?:')})`, 'gi');
+    return string.replace(re, match => {
+      let out = map[match.toLowerCase()];
+      // Not very tidy way to work out if the word is capitalised
+      if ((match.match(/[A-Z]/g) || []).length > match.length / 2) {
+        out = out.toUpperCase();
+      }
+      return out;
+    });
+  }
+
+  string = replaceAll(string, words);
+
+  // OwO
+  string = string.replace(/[rl]/gi, match =>
+    match.charCodeAt(0) < 97 ? 'W' : 'w'
+  );
+
+  // Nya >;3
+  string = string.replace(/n[aeiou]/gi, match =>
+    `${match[0]}${match.charCodeAt(1) < 97 ? 'Y' : 'y'}${match[1]}`
+  );
+
+  // Words that end in y like cummy wummy
+  string = string.replace(/\b[A-V,X-Z,a-v,x-z]\w{3,}y\b/gi, match =>
+    `${match} ${match.charCodeAt(0) < 97 ? 'W' : 'w'}${match.slice(1)}`
+  );
+
+  // S-stutter
+  string = string.split(' ').map(word => {
+    if (word.length === 0 || word[0].match(/[a-zA-Z]/) === null) {
+      return word;
+    }
+
+    while (Math.random() < stutterChance) {
+      word = `${word[0]}-${word}`;
+    }
+
+    return word;
+  }).join(' ');
+  // Prefixes
+  if (Math.random() < prefixChance) {
+    string = `${string} ${suffixes[Math.floor(Math.random() * suffixes.length)]}`;
+  }
+
+  // Suffixes
+  if (Math.random() < suffixChance) {
+    string = `${prefixes[Math.floor(Math.random() * prefixes.length)]} ${string}`;
+  }
+
+  return string;
+};
