79228ab4bbf22ea748da8aab07679860cee2f6af
dperolio <dustin.perolio@outlook.com>
2021-01-10 04:35:18 -0500

    Add millisecondsToTime time util

--- src/core/modules/util/Time.js
+++ src/core/modules/util/Time.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions *//* eslint-disable no-unused-vars */
+import { getModule } from '../webpack';
 
 /**
  * @module util.time
@@ -7,6 +8,24 @@
  * @version 0.0.1
  */
 
+export const millisecondsToTime = ms => {
+  const time = getModule('momentProperties').duration(ms);
+  let output;
+  output = time.hours() ? time.hours() : null;
+
+  if (time.minutes()) {
+    output = output ? `${output}:${time.minutes()}` : time.minutes();
+    if (time.seconds()) {
+      output = output ? `${output}:${time.seconds()}` : time.seconds();
+    } else {
+      output = output ? `${output}:00` : '00';
+    }
+  } else {
+    output = output ? `${output}:00` : '00';
+  }
+
+  return output;
+};
 
 export const add = date => {
   return void 0;
