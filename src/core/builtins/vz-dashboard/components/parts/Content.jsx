6e8bb4843e35b70741b6fbfc8ddab681f71f0426
dperolio <dustin.perolio@outlook.com>
2021-01-14 21:33:18 -0500

    Fix adding page attributes to addon/builtin routes.

--- src/core/builtins/vz-dashboard/components/parts/Content.jsx
+++ src/core/builtins/vz-dashboard/components/parts/Content.jsx
@@ -4,15 +4,27 @@ import { Icon, Divider } from '@vizality/components';
 import { joinClassNames } from '@vizality/util/dom';
 import { getModule } from '@vizality/webpack';
 
-export default memo(({ icon, heading, subheading, className, children }) => {
-  const { base } = getModule('base');
-  const { content } = getModule('wrappedLayout');
+export default memo(props => {
+  const { icon, heading, subheading, className, children } = props;
+
+  /**
+   * @note Deleting these props as we've already stored their values and we don't want
+   * to send them to the content div when we use spread props below.
+   */
+  delete props.icon;
+  delete props.heading;
+  delete props.subheading;
+  delete props.className;
+  delete props.children;
+
   const { marginBottom20 } = getModule('marginBottom20');
-  const { h1 } = getModule('h1', 'h2', 'h3');
   const { headerSubtext } = getModule('headerSubtext');
+  const { content } = getModule('wrappedLayout');
+  const { h1 } = getModule('h1', 'h2', 'h3');
+  const { base } = getModule('base');
 
   return (
-    <div className={joinClassNames('vz-dashboard-content', className)}>
+    <div className={joinClassNames('vz-dashboard-content', className)} {...props}>
       {heading && <div className={`vz-dashboard-content-header-wrapper ${marginBottom20}`}>
         <div className='vz-dashboard-content-header-inner-wrapper'>
           {icon && <div className='vz-dashboard-content-header-icon-wrapper'>
