d42a5c23c078ffe80f5549ad88e790b264b54480
dperolio <dustin.perolio@outlook.com>
2021-01-16 13:40:16 -0500

    Update hooks and hooks jsdoc

--- src/core/modules/hooks/index.js
+++ src/core/modules/hooks/index.js
@@ -1,33 +1,92 @@
 import { useLayoutEffect, useEffect, useState, useRef } from 'react';
 
+import { get, post, put, del } from '../http';
 import { getObjectURL } from '../util/File';
-import { get } from '../http';
 
 /**
  * Hook that uses util.file.getObjectURL to get an async collection of blob object URLs.
+ * @param {string} requestType Request type. One of get, post, put, or del.
+ * @param {url} url URL to call
+ * @param {string|object} [headers] Headers
  */
-export function useFetch (url, opts) {
+export function useFetch (requestType, url, headers) {
   const [ response, setResponse ] = useState(null);
   const [ loading, setLoading ] = useState(true);
   const [ hasError, setHasError ] = useState(false);
 
   useEffect(() => {
-    get(url, opts)
-      .then(res => {
-        setResponse(res.data);
-        setLoading(false);
-      })
-      .catch(() => {
-        setHasError(true);
-        setLoading(false);
-      });
+    switch (requestType.toLowerCase()) {
+      case 'get':
+        return get(url, headers)
+          .then(res => {
+            setResponse(res.data);
+            setLoading(false);
+          })
+          .catch(() => {
+            setHasError(true);
+            setLoading(false);
+          });
+      case 'post':
+        return post(url, headers)
+          .then(res => {
+            setResponse(res.data);
+            setLoading(false);
+          })
+          .catch(() => {
+            setHasError(true);
+            setLoading(false);
+          });
+      case 'put':
+        return put(url, headers)
+          .then(res => {
+            setResponse(res.data);
+            setLoading(false);
+          })
+          .catch(() => {
+            setHasError(true);
+            setLoading(false);
+          });
+      case 'del':
+        return del(url, headers)
+          .then(res => {
+            setResponse(res.data);
+            setLoading(false);
+          })
+          .catch(() => {
+            setHasError(true);
+            setLoading(false);
+          });
+    }
   }, [ url ]);
 
   return [ response, loading, hasError ];
 }
 
 /**
- * Hook that uses util.file.getObjectURL to get an async collection of blob object URLs.
+ * Hook that uses `util.file.getObjectURL` to get an async collection of blob object URLs.
+ * @param {string} path Path to the file or directory to be included
+ * @param {string|Array} [allowedExtensions] List of desired file-type extensions to be included
+ * @example
+ * ```
+ * const [ images, loading, error ] = useFetchImageObjectURL(dir);
+ * return (
+ *   {error && 'Uhoh, something went wrong.'}
+ *   {loading && <Spinner />}
+ *   {images && images.length &&
+ *     <SomeReallyCoolComponent>
+ *       <SomeChidren />
+ *     </SomeReallyCoolComponent>
+ *   }
+ * )
+ * ```
+ * @note You will still need to revoke the URLs from this. A good idea is usually to revoke on unmount
+ * which can be done as such:
+ * @example
+ * ```
+ * useEffect(() => {
+ *   return () => images && images.forEach(image => URL.revokeObjectURL(image.url));
+ * }, [ images ]);
+ * ```
  */
 export function useFetchImageObjectURL (path, allowedExtensions) {
   const [ response, setResponse ] = useState(null);
@@ -51,8 +110,15 @@ export function useFetchImageObjectURL (path, allowedExtensions) {
 
 /**
  * Simple hook to force a component to rerender.
- * @copyright MIT, Hovhannes Babayan 2019
+ * @copyright Copyright (c) 2019 Hovhannes Babayan
+ * @license MIT
  * @see {@link https://github.com/bhovhannes/use-force-update-hook}
+ * @example
+ * ```
+ * const forceUpdate = useForceUpdate();
+ * ...
+ * forceUpdate();
+ * ```
  */
 export function useForceUpdate () {
   const setValue = useState(0)[1];
@@ -62,9 +128,26 @@ export function useForceUpdate () {
 /**
  * In rare cases you may need to do something right after component forceUpdate finishes.
  * In that case useForceUpdateWithCallback can be useful.
- * @copyright MIT, Hovhannes Babayan 2019
+ * @copyright Copyright (c) 2019 Hovhannes Babayan
+ * @license MIT
  * @see {@link https://github.com/bhovhannes/use-force-update-hook}
- * @param {*} callback Callback
+ * @param {*} [callback] Callback
+ * @example
+ * ```
+ * function handleUpdate () {
+ *   console.log('Just updated.')
+ * }
+ *
+ * const forceUpdate = useForceUpdateWithCallback(handleUpdate)
+ *
+ * return (
+ *   <div>
+ *     <button onClick={forceUpdate}>
+ *       Click to rerender MyAwesomeComponent
+ *     </button>
+ *   </div>
+ * );
+ * ```
  */
 export function useForceUpdateWithCallback (callback) {
   const [ value, setValue ] = useState(0);
