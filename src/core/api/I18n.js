a6e6a7390925340e5848761226749adde70b0489
dperolio <dustin.perolio@outlook.com>
2021-01-21 18:33:30 -0500

    Update api.i18n, fix stop on it

--- src/core/api/I18n.js
+++ src/core/api/I18n.js
@@ -1,45 +1,46 @@
-import { Directories } from '@vizality/constants';
 import { getModule } from '@vizality/webpack';
 import { API } from '@vizality/entities';
 import i18n from '@vizality/i18n';
+
+import * as strings from '../languages';
 
 export default class I18n extends API {
   constructor () {
     super();
     this.messages = {};
     this.locale = null;
+    this._vizality = strings;
+    this.injectAllStrings(this._vizality);
     this._module = 'API';
     this._submodule = 'I18n';
-
-    (async () => {
-      this.strings = await import(Directories.LANGUAGES);
-      this.injectAllStrings(this.strings);
-    })();
   }
 
   async start () {
     await getModule('locale', 'theme', true).then(module => {
       this.locale = module.locale;
+
       module.addChangeListener(() => {
         if (module.locale !== this.locale) {
           this.locale = module.locale;
           i18n.loadPromise.then(() => this._addVizalityStrings());
         }
       });
+
       this._addVizalityStrings();
     });
   }
 
   stop () {
+    /*
+     * @todo There is a problem with plugins that override default strings.
+     * This will remove those as well, causing blank strings and possibly crashes.
+     * Need to store the original strings somewhere when overriding in order to revert
+     * on stop. Maybe this._original?
+     */
     [ 'messages', 'defaultMessages' ].forEach(obj => {
       Object.keys(i18n._proxyContext[obj])
-        .filter(key =>
-          Object.keys(this.messages[this.locale])
-            .filter(k => k === key))
-        .forEach(key => {
-          console.log('key2', key);
-          delete i18n._proxyContext[obj][key];
-        });
+        .filter(key => Object.keys(this.messages[this.locale]).filter(k => k === key))
+        .forEach(key => delete i18n._proxyContext[obj][key]);
     });
   }
 
@@ -60,13 +61,19 @@ export default class I18n extends API {
 
   injectAllStrings (strings) {
     Object.keys(strings).forEach(locale => {
-      const ogLocale = locale;
-      locale = (/-/).test(locale) ? ogLocale : locale.replace(/([A-Z])/, '-$1').trim();
-      this.injectStrings(locale, strings[ogLocale]);
+      this.injectStrings(locale, strings[locale]);
     });
   }
 
   injectStrings (locale, strings) {
+    /**
+     * @note Because you can't easily export modules with hyphens in them, this
+     * account for plugins using require and plugins using ESM syntax, and the
+     * possibility they may export without the hyphen.
+     */
+    const ogLocale = locale;
+    locale = (/-/).test(locale) ? ogLocale : locale.replace(/([A-Z])/, '-$1').trim();
+
     if (!this.messages[locale]) {
       this.messages[locale] = strings;
     } else {
