3e8a596cb24b2af8cbc4c6f5870e18f8a35f2311
dperolio <dustin.perolio@outlook.com>
2021-01-23 01:52:49 -0500

    Add vz-type attribute to special message types (thanks for pointing out the need Winston)

--- src/core/builtins/vz-attributes/modules/chat/Message.js
+++ src/core/builtins/vz-attributes/modules/chat/Message.js
@@ -21,7 +21,6 @@ export default () => {
     res.props.children.props['vz-bot-vizality'] = Boolean(message?.author?.phone === toHash('VIZALITY') && message?.author?.avatar !== 'clyde') && '';
     res.props.children.props['vz-bot-plugin'] = Boolean(message?.author?.phone === toHash('PLUGIN') && message?.author?.avatar !== 'clyde') && '';
     res.props.children.props['vz-user-id'] = new RegExp(Regexes.USER_ID).test(message?.author?.id) && message?.author?.id;
-    res.props.children.props['vz-system'] = Boolean(message?.type && message?.type === 6) && '';
     res.props.children.props['vz-local'] = Boolean(message?.author?.isLocalBot()) && '';
     res.props.children.props['vz-mentioned'] = Boolean(message?.mentioned) && '';
     res.props.children.props['vz-self'] = Boolean(message?.author?.email) && '';
@@ -42,6 +41,28 @@ export default () => {
     res.props.children.props['vz-embed'] = Boolean(message?.embeds?.length) && '';
     res.props.children.props['vz-blocked'] = Boolean(message?.blocked) && '';
     res.props.children.props['vz-message-id'] = message?.id;
+
+    /**
+     * Set an attribute for special types of messages.
+     * @see {@link https://discord.com/developers/docs/resources/channel#message-object-message-types}
+     */
+    res.props.children.props['vz-type'] =
+      (Boolean(message?.type && message?.type === 1) && 'recipient-add') ||
+      (Boolean(message?.type && message?.type === 2) && 'recipient-remove') ||
+      (Boolean(message?.type && message?.type === 3) && 'call') ||
+      (Boolean(message?.type && message?.type === 4) && 'channel-name-change') ||
+      (Boolean(message?.type && message?.type === 5) && 'channel-icon-change') ||
+      (Boolean(message?.type && message?.type === 6) && 'pin') ||
+      (Boolean(message?.type && message?.type === 7) && 'member-join') ||
+      (Boolean(message?.type && message?.type === 8) && 'server-boost') ||
+      (Boolean(message?.type && message?.type === 9) && 'server-boost-tier-1') ||
+      (Boolean(message?.type && message?.type === 10) && 'server-boost-tier-2') ||
+      (Boolean(message?.type && message?.type === 11) && 'server-boost-tier-3') ||
+      (Boolean(message?.type && message?.type === 12) && 'follow') ||
+      (Boolean(message?.type && message?.type === 14) && 'discovery-disqualified') ||
+      (Boolean(message?.type && message?.type === 15) && 'discovery-requalified') ||
+      (Boolean(message?.type && message?.type === 19) && 'reply') ||
+      (Boolean(message?.type && message?.type === 20) && 'application-command');
 
     return res;
   });
