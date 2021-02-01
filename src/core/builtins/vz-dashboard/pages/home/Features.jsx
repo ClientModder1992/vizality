e644388409e550ca1490dc7d345b932c4f83e931
dperolio <dustin.perolio@outlook.com>
2021-02-01 16:19:36 -0500

    Move dashboard pages dir out of components dir

--- src/core/builtins/vz-dashboard/pages/home/Features.jsx
+++ src/core/builtins/vz-dashboard/pages/home/Features.jsx
@@ -36,28 +36,28 @@ export default memo(() => {
           header='Discover Plugins'
           description='Explore our vast collection of official plugins. Plugins can add just about any new feature you can imagine to Discord.'
           buttonText='Browse Plugins'
-          path='/vizality/dashboard/plugins/discover'
+          path='/vizality/plugins/discover'
         />
         <Card
           icon='Theme'
           header='Discover Themes'
           description='Explore our vast collection of official themes. Themes can change just small portions of the app or they can be full-blown redesigns.'
           buttonText='Browse Themes'
-          path='/vizality/dashboard/themes/discover'
+          path='/vizality/themes/discover'
         />
         <Card
           icon='UnknownUser'
           header='Become a Developer'
           description='Find out what it takes to start making your own plugins and themes. Come check out the guidelines and all the info you need to become an addon developer for Vizality.'
           buttonText='Learn How'
-          path='/vizality/dashboard/developers'
+          path='/vizality/developers'
         />
         <Card
           icon='Science'
           header='Read the Docs'
           description='Read about all the building blocks and tools we provide to make building plugins and themes easier for you.'
           buttonText='Start Reading'
-          path='/vizality/dashboard/documentation'
+          path='/vizality/docs'
         />
       </div>
     </div>
