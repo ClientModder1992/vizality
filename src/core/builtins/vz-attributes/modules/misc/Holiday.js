1110114f4e26cfc871eb1a36aa936967e32510ef
dperolio <dustin.perolio@outlook.com>
2021-02-01 21:25:56 -0500

    Fix months in vz-holiday attribute since they start with 0

--- src/core/builtins/vz-attributes/modules/misc/Holiday.js
+++ src/core/builtins/vz-attributes/modules/misc/Holiday.js
@@ -2,10 +2,10 @@ export default () => {
   const root = document.documentElement;
   const date = new Date();
 
-  if (date.getMonth() === 1 && date.getDate() === 1) root.setAttribute('vz-holiday', 'new-years');
-  if (date.getMonth() === 3 && date.getDate() === 1) root.setAttribute('vz-holiday', 'april-fools');
-  if (date.getMonth() === 10 && date.getDate() === 31) root.setAttribute('vz-holiday', 'halloween');
-  if (date.getMonth() === 12 && date.getDate() === 25) root.setAttribute('vz-holiday', 'christmas');
+  if (date.getMonth() === 0 && date.getDate() === 1) root.setAttribute('vz-holiday', 'new-years');
+  if (date.getMonth() === 2 && date.getDate() === 1) root.setAttribute('vz-holiday', 'april-fools');
+  if (date.getMonth() === 9 && date.getDate() === 31) root.setAttribute('vz-holiday', 'halloween');
+  if (date.getMonth() === 11 && date.getDate() === 25) root.setAttribute('vz-holiday', 'christmas');
 
   return () => {
     root.removeAttribute('vz-holiday');
