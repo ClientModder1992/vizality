004acc50eba798e85013eb15dcd67f6b5ce7e2ec
dperolio <dustin.perolio@outlook.com>
2021-02-01 21:25:30 -0500

    Add a check to role headers to fix an error

--- src/core/builtins/vz-attributes/modules/members/RoleHeader.js
+++ src/core/builtins/vz-attributes/modules/members/RoleHeader.js
@@ -3,11 +3,15 @@ import { patch, unpatch } from '@vizality/patcher';
 import { getModule } from '@vizality/webpack';
 
 export default () => {
-  const MembersGroup = getModule(m => m.default?.displayName === 'ListSectionItem');
+  const ListSectionItem = getModule(m => m.default?.displayName === 'ListSectionItem');
   const { membersGroup } = getModule('membersGroup');
 
-  patch('vz-attributes-members-role-headers', MembersGroup, 'default', (_, res) => {
-    if (!res.props?.className.includes(membersGroup)) return res;
+  patch('vz-attributes-members-role-headers', ListSectionItem, 'default', (_, res) => {
+    /*
+     * ListSectionItem is used for more than just channel member role headers, so let's make sure
+     * we're targetting just the channel member role headers here.
+     */
+    if (!res.props?.children?.props?.children || !res.props?.className?.includes(membersGroup)) return res;
 
     [ res.props['vz-role-name'] ] = res.props?.children?.props?.children;
     [ , , res.props['vz-online-count'] ] = res.props?.children?.props?.children;
