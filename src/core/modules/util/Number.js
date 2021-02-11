7d3a33cfc6e48d4e60423cbeb03610e991c3fbad
dperolio <dustin.perolio@outlook.com>
2021-02-11 16:48:12 -0500

    Add number utils getAverage and getMedian

--- src/core/modules/util/Number.js
+++ src/core/modules/util/Number.js
@@ -12,3 +12,35 @@ export const isNumber = input => {
 export const assertNumber = input => {
   return void 0 || input;
 };
+
+/**
+ * Gets the average value from a set of numbers.
+ * Sourced from @see {@link https://stackoverflow.com/a/45309555|StackOverflow}
+ * @param {Array<number>} numbers An array of numbers.
+ * @returns {number} The average of the numbers in the array.
+ */
+export const getAverage = numbers => {
+  assertArray(numbers);
+  return numbers.reduce((all, one, _, src) => all += one / src.length, 0);
+};
+
+/**
+ * Gets the median value from a set of numbers.
+ * Sourced from @see {@link https://stackoverflow.com/a/45309555|StackOverflow}
+ * @param {Array<number>} numbers An array of numbers.
+ * @returns {number} The median of the numbers in the array.
+ */
+export const getMedian = numbers => {
+  assertArray(numbers);
+
+  numbers.sort((a, b) => {
+    return a - b;
+  });
+
+  const half = Math.floor(numbers.length / 2);
+  if (numbers.length % 2) {
+    return numbers[half];
+  }
+
+  return (numbers[half - 1] + numbers[half]) / 2.0;
+};
