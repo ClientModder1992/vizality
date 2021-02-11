101ddddfdc31c4119c8efaf77f06a9b2a2406465
dperolio <dustin.perolio@outlook.com>
2021-02-11 16:29:47 -0500

    Remove window util and move captureElement method to dom util

--- src/core/modules/util/DOM.js
+++ src/core/modules/util/DOM.js
@@ -112,3 +112,22 @@ export const joinClassNames = (...items) => {
   }
   return classes.join(' ');
 };
+
+// @todo Figure out why this isn't working for Windows users.
+export const captureElement = async selector => {
+  return (await (() => {
+    const getSources = getModule('DesktopSources', 'default').default;
+    const mediaEngine = getModule('getMediaEngine').getMediaEngine();
+    async function capture (selector) {
+      const el = document.querySelector(selector);
+      const elRect = el.getBoundingClientRect();
+      const sources = await getSources(mediaEngine, [ 'window' ], { width: window.outerWidth, height: window.outerHeight });
+      const discord = sources.find(src => src.name === `${document.title} - Discord` || src.name === 'Discord');
+      const img = nativeImage.createFromDataURL(discord.url);
+      return img.crop(elRect);
+    }
+
+    return capture(selector);
+  })()).toDataURL();
+};
+
