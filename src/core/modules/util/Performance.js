0e4911a7a82a56f82c3a97384c5ee7b9b4a9fe62
dperolio <dustin.perolio@outlook.com>
2021-02-11 16:33:56 -0500

    Revamp performance util benchmark method (thanks Kyza)

--- src/core/modules/util/Performance.js
+++ src/core/modules/util/Performance.js
@@ -9,100 +9,67 @@ import { performance, PerformanceObserver } from 'perf_hooks';
  * @param {boolean} [log=true] Whether or not to log the results to the console right away
  * @returns {object} The results. All ready to be handled if you don't want them printed to the console.
  */
-export const benchmark = (funcs, iterations, nano = true, log = true) => {
-  return new Promise(async resolve => {
-    const res = [];
 
-    for (let i = 0; i < funcs.length; i++) {
-      const funct = funcs[i];
 
-      let obs;
-      try {
-        const benches = [];
 
-        await new Promise(resolve => {
-          const wrapper = performance.timerify(funct);
-          obs = new PerformanceObserver((list) => {
+/**
+ * Runs a function or set over function a specified number of times and then reports
+ * the average and median run times of each function, and if multiple functions are specified
+ * it shows a comparison table of each function's performance.
+ * * Sourced from @see {@link https://github.com/Kyza/ittai/blob/75039f97dcfda4fc80690b00ff8a319fb0539fa0/core/utils/index.js#L176}
+ * @param {Function|Array<Function>} code Code to benchmark.
+ * @param {number} [iterations=1000] Amount of times to run the benchmark
+ * @param {boolean} [nonoseconds=false] Whether to show output in nanoseconds or milliseconds
+ * @param {boolean} [showLog=true] Whether to show a console log of the benchmark
+ * @returns {Promise} A promise that resolves when the benchmark is completed
+ */
+export const benchmark = (code, iterations = 1000, nonoseconds = false, showLog = true) => {
+  try {
+    return new Promise((resolve) => {
+      const pre = code.pre ?? (() => {});
+      delete code.pre;
+      const post = code.post ?? (() => {});
+      delete code.post;
+
+      const name = Object.keys(code)[0];
+      code = code[Object.keys(code)[0]];
+      const promises = [];
+
+      const toNanoseconds = () => {
+        const hrTime = process.hrtime();
+        return hrTime[0] * 1000000000 + hrTime[1];
+      };
+
+      for (let i = 0; i < iterations; i++) {
+        promises.push(
+          new Promise((resolve) => {
+            let returns, start, end;
             try {
-              benches.push(
-                BigInt(
-                  (
-                    list.getEntries()[
-                      list.getEntries().length - 1
-                    ].duration * 1000000
-                  ).toFixed(0)
-                )
-              );
-
-              if (benches.length === iterations) {
-                obs.disconnect();
-                resolve();
-              }
-            } catch {
-              obs.disconnect();
+              pre();
+              start = toNanoseconds();
+              returns = code();
+              end = toNanoseconds();
+              post();
+            } catch (err) {
+              resolve({ returns, time: 0, error: err });
             }
-          });
-          obs.observe({ entryTypes: [ 'function' ] });
-          for (let j = 0; j < iterations; j++) {
-            wrapper();
-          }
-        });
-
-        const getMedian = array => {
-          const copy = BigInt64Array.from(array).sort();
-          return copy[Math.floor(copy.length / 2)];
-        };
-
-        const getAverage = array => {
-          return BigInt(array.reduce((a, b) => a + b) / BigInt(array.length));
-        };
-
-        /*
-         * The PerformanceObserver only sees up to 100 nanoseconds of precision.
-         * Round to the nearest 100s place to easily ignore rounding errors.
-         */
-        const median = Math.round(Number(getMedian(benches)) / 100) * 100;
-        const average = getAverage(benches);
-
-        res.push({
-          Iterations: iterations,
-          'Median Time': nano
-            ? `${median.toLocaleString()}ns`
-            : `${(median / 1000000n).toLocaleString()}ms`,
-          'Average Time': nano
-            ? `${average.toLocaleString()}ns`
-            : `${(average / 1000000n).toLocaleString()}ms`,
-          Function: funct.toString(),
-          '(Median Time)': median,
-          '(Average Time)': average
-        });
-      } catch (error) {
-        try {
-          obs.disconnect();
-        } catch {}
-        res.push({
-          'Median Time': null,
-          Function: funct.toString(),
-          Error: error
-        });
-        console.error(error);
+            resolve({ returns, time: end - start, error: false });
+          })
+        );
       }
-    }
 
-    res.sort((bench1, bench2) => {
-      if (bench1['(Median Time)'] > bench2['(Median Time)']) return 1;
-      if (bench1['(Median Time)'] < bench2['(Median Time)']) return -1;
-      return 0;
+      Promise.all(promises).then((allReturns) => {
+        const finalTimes = allReturns.map((r) => r.time);
+        resolve({
+          name,
+          average: getAverage(finalTimes),
+          median: getMedian(finalTimes),
+          error: allReturns[0].error,
+          returns: allReturns[0].returns
+        });
+      });
     });
-
-    if (log) {
-      console.table(res, [
-        'Median Time',
-        'Average Time',
-        'Function',
-        'Error'
-      ]);
-    }
-    resolve(res);
-  });
+  } catch (err) {
+    return _error(err);
+  }
 };
