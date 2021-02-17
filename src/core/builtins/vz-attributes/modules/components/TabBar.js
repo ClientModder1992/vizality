cb1e8ec03cb2c10112020e0894602fa81dd99086
dperolio <dustin.perolio@outlook.com>
2021-02-17 16:55:50 -0500

    Change builtin attributes on TabBar
    - vz-item-selected changed to vz-tab-selected
    - vz-item changed to vz-tab
    - vz-active added

--- src/core/builtins/vz-attributes/modules/components/TabBar.js
+++ src/core/builtins/vz-attributes/modules/components/TabBar.js
@@ -4,30 +4,26 @@ import { patch, unpatch } from '@vizality/patcher';
 
 /*
  * Modifies The TabBar component, which is used in various places throughout
- * Discord (settings sidebar, friends list tabs, user profile tabs). This adds
- * classes to those items to make them more easy / consistent to target.
+ * Discord (settings sidebar, friends list tabs, user profile tabs).
+ * This adds classes to those items to make them more easy / consistent to target.
  */
 export default () => {
   const TabBar = getModuleByDisplayName('TabBar');
-
   patch('vz-attributes-tab-bar', TabBar.prototype, 'render', function (_, res) {
     const { selectedItem } = this.props;
-
-    res.props['vz-item-selected'] = toKebabCase(selectedItem);
-
+    res.props['vz-tab-selected'] = toKebabCase(selectedItem);
     return res;
   });
 
-  patch('vz-attributes-tab-bar-items', TabBar.Item?.prototype, 'render', function (_, res) {
-    const { id } = this.props;
-
-    res.props['vz-item'] = toKebabCase(id);
-
+  patch('vz-attributes-tab-bar-tabs', TabBar.Item?.prototype, 'render', function (_, res) {
+    const { id, selectedItem } = this.props;
+    res.props['vz-active'] = Boolean(id?.toLowerCase() === selectedItem?.toLowerCase()) && '';
+    res.props['vz-tab'] = toKebabCase(id);
     return res;
   });
 
   return () => {
     unpatch('vz-attributes-tab-bar');
-    unpatch('vz-attributes-tab-bar-items');
+    unpatch('vz-attributes-tab-bar-tabs');
   };
 };
