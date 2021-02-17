e09daa2ffb0ffdddb4f6ce192b94ffb39c8bcfd7
dperolio <dustin.perolio@outlook.com>
2021-02-17 16:54:06 -0500

    RPC api and builtin fixed up (thanks AA <3)

--- src/core/api/RPC.js
+++ src/core/api/RPC.js
@@ -2,81 +2,109 @@ import { API } from '@vizality/entities';
 
 /**
  * @typedef DiscordRpcEvent
- * @todo Dig into discord's more complex selectors (seem to be mongo-ish)
+ * @todo Dig into Discord's more complex selectors (seem to be mongo-ish).
  * @todo Document validation field
- * @property {String} scope RPC scope required
- * @property {function(Object): void} handler RPC event handler
+ * @property {string} scope RPC scope required
+ * @property {function(object): void} handler RPC event handler
  * @property {any|undefined} validation Validator for incoming data
  */
 
 /**
- * API to tinker with Discord's RPC socket
- * @property {Object.<String, function(String): Boolean>} scopes RPC Scopes
- * @property {Object.<String, DiscordRpcEvent>} scopes RPC Scopes
+ * API to tinker with Discord's RPC socket.
+ * @property {object.<string, function(string): boolean>} scopes RPC Scopes
+ * @property {object.<string, DiscordRpcEvent>} scopes RPC Scopes
  */
 export default class RPC extends API {
   constructor () {
     super();
     this.scopes = {};
     this.events = {};
+    this.commands = {};
     this._module = 'API';
     this._submodule = 'RPC';
   }
 
+  stop () {
+    delete vizality.api.rpc;
+    this.removeAllListeners();
+  }
+
   /**
-   * Registers a RPC scope
-   * @param {String} scope RPC scope
-   * @param {function(String): Boolean} grant Grant method. Receives the origin as first argument
-   * @emits RpcAPI#scopeAdded
+   * Registers a RPC scope.
+   * @param {string} scope RPC scope
+   * @param {function(string): boolean} grant Grant method. Receives the origin as first argument
+   * @emits RpcAPI#scopeAdd
    */
   registerScope (scope, grant) {
     if (this.scopes[scope]) {
       throw new Error(`RPC scope ${scope} is already registered!`);
     }
     this.scopes[scope] = grant;
-    this.emit('scopeAdded', scope);
+    this.emit('scopeAdd', scope);
   }
 
   /**
-   * Registers a RPC event
-   * @param {String} name Event name
+   * Registers an RPC event.
+   * @param {string} name Event name
    * @param {DiscordRpcEvent} properties RPC event properties
-   * @emits RpcAPI#eventAdded
+   * @emits RpcAPI#eventAdd
    */
   registerEvent (name, properties) {
     if (this.events[name]) {
       throw new Error(`RPC event ${name} is already registered!`);
     }
+    if (!properties.handler) properties.handler = () => {}
     this.events[name] = properties;
-    this.emit('eventAdded', name);
+    this.emit('eventAdd', name);
   }
 
   /**
-   * Unregisters a scope
-   * @param {String} scope Scope to unregister
-   * @emits RpcAPI#scopeRemoved
+   * Registers an RPC command.
+   * @param {string} name Command name
+   * @param {DiscordRpcEvent} properties RPC command properties
+   * @emits RpcAPI#eventAdd
+   */
+  registerCommand (name, properties) {
+    if (this.commands[name]) {
+      throw new Error(`RPC command ${name} is already registered!`);
+    }
+    this.commands[name] = properties;
+    this.emit('commandAdd', name);
+  }
+
+  /**
+   * Unregisters a scope.
+   * @param {string} scope Scope
+   * @emits RpcAPI#scopeRemove
    */
   unregisterScope (scope) {
     if (this.scopes[scope]) {
       delete this.scopes[scope];
-      this.emit('scopeRemoved', scope);
+      this.emit('scopeRemove', scope);
     }
   }
 
   /**
-   * Unregisters an event
-   * @param {String} name Name of the event to unregister
-   * @emits RpcAPI#eventRemoved
+   * Unregisters an event.
+   * @param {string} name Event name
+   * @emits RpcAPI#eventRemove
    */
   unregisterEvent (name) {
     if (this.events[name]) {
       delete this.events[name];
-      this.emit('eventRemoved', name);
+      this.emit('eventRemove', name);
     }
   }
 
-  stop () {
-    delete vizality.api.rpc;
-    this.removeAllListeners();
+  /**
+   * Unregisters a command.
+   * @param {string} name Command name
+   * @emits RpcAPI#commandRemove
+   */
+  unregisterCommand (name) {
+    if (this.commands[name]) {
+      delete this.commands[name];
+      this.emit('commandRemove', name);
+    }
   }
 }
--- src/core/builtins/vz-rpc/index.js
+++ src/core/builtins/vz-rpc/index.js
@@ -5,21 +5,54 @@ import { HTTP } from '@vizality/constants';
 
 export default class RPC extends Builtin {
   start () {
-    this.handlers = getModule('commands');
+    this.handlers = getModule('events', 'commands');
     this._patchWebSocketServer();
 
     this._boundAddEvent = this._addEvent.bind(this);
+    this._boundAddCommand = this._addCommand.bind(this);
     this._boundRemoveEvent = this._removeEvent.bind(this);
+    this._boundRemoveCommand = this._removeCommand.bind(this);
 
     vizality.api.rpc.registerScope('VIZALITY_PRIVATE', w => w === HTTP.WEBSITE);
-    vizality.api.rpc.on('eventAdded', this._boundAddEvent);
-    vizality.api.rpc.on('eventRemoved', this._boundRemoveEvent);
+    vizality.api.rpc.on('eventAdd', this._boundAddEvent);
+    vizality.api.rpc.on('commandAdd', this._boundAddCommand);
+    vizality.api.rpc.on('eventRemove', this._boundRemoveEvent);
+    vizality.api.rpc.on('commandRemove', this._boundRemoveCommand);
 
-    vizality.api.rpc.registerEvent('IS_VIZALITY', {
+    vizality.api.rpc.registerCommand('IS_VIZALITY', {
       scope: 'VIZALITY_PRIVATE',
-      handler: e => {
-        // console.log(e);
-        // return 'yes';
+      handler: () => {
+        return 'true';
+      }
+    });
+
+    vizality.api.rpc.registerCommand('VIZALITY_GET_INFO', {
+      scope: 'VIZALITY_PRIVATE',
+      handler: () => {
+        return {
+          git: vizality.git,
+          plugins: vizality.manager.plugins.keys.map(k => ({
+            manifest: vizality.manager.plugins.get(k).manifest,
+            addonId: k,
+            enabled: vizality.manager.plugins.isEnabled(k)
+          })),
+          themes: vizality.manager.themes.keys.map(k => ({
+            manifest: vizality.manager.themes.get(k).manifest,
+            addonId: k,
+            enabled: vizality.manager.themes.isEnabled(k)
+          })),
+          builtins: vizality.manager.builtins.keys.map(k => ({
+            addonId: k,
+            enabled: vizality.manager.builtins.isEnabled(k)
+          }))
+        };
+      }
+    });
+
+    vizality.api.rpc.registerCommand('VIZALITY_NAVIGATE', {
+      scope: 'VIZALITY_PRIVATE',
+      handler: evt => {
+        return vizality.api.routes.navigateTo(evt.args);
       }
     });
   }
@@ -28,17 +61,20 @@ export default class RPC extends Builtin {
     unpatch('vz-rpc-webSocket');
     unpatch('vz-rpc-webSocket-promise');
 
-    vizality.api.rpc.unregisterEvent('IS_VIZALITY');
     vizality.api.rpc.unregisterScope('VIZALITY_PRIVATE');
-    vizality.api.rpc.off('eventAdded', this._boundAddEvent);
-    vizality.api.rpc.off('eventRemoved', this._boundRemoveEvent);
+    vizality.api.rpc.unregisterCommand('IS_VIZALITY');
+    vizality.api.rpc.unregisterCommand('VIZALITY_GET_INFO');
+    vizality.api.rpc.unregisterCommand('VIZALITY_NAVIGATE');
+
+    vizality.api.rpc.off('eventAdd', this._boundAddEvent);
+    vizality.api.rpc.off('commandAdd', this._boundAddCommand);
+    vizality.api.rpc.off('eventRemove', this._boundRemoveEvent);
+    vizality.api.rpc.off('commandRemove', this._boundRemoveCommand);
   }
 
   async _patchWebSocketServer () {
     const websocketHandler = getModule('validateSocketClient');
-
     patch('vz-rpc-webSocket', websocketHandler, 'validateSocketClient', args => {
-      // console.log(args);
       if (args[2] === 'vizality') {
         args[2] = void 0;
         args[3] = 'vizality';
@@ -64,15 +100,15 @@ export default class RPC extends Builtin {
     this.handlers.events[event] = vizality.api.rpc.events[event];
   }
 
+  _addCommand (command) {
+    this.handlers.commands[command] = vizality.api.rpc.commands[command];
+  }
+
   _removeEvent (event) {
     delete this.handlers.events[event];
   }
 
-  _addCommands (event) {
-    this.handlers.commands[event] = vizality.api.rpc.events[event];
-  }
-
-  _removeCommands (event) {
-    delete this.handlers.commands[event];
+  _removeCommand (command) {
+    delete this.handlers.commands[command];
   }
 }
