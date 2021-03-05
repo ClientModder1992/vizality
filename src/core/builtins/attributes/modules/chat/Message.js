f159c9c01517e7b3367cd4420bc7abd5643e0607
Strencher <46447572+Strencher@users.noreply.github.com>
2021-03-05 13:27:02 +0100

    refactor message attributes with better error log

--- src/core/builtins/attributes/modules/chat/Message.js
+++ src/core/builtins/attributes/modules/chat/Message.js
@@ -6,11 +6,63 @@ import { Regexes } from '@vizality/constants';
 
 export const labels = [ 'Chat' ];
 
+const messageTypes = {
+  1: 'recipient-add',
+  2: 'recipient-remove',
+  3: 'call',
+  4: 'channel-name-change',
+  5: 'channel-icon-change',
+  6: 'pin',
+  7: 'member-join',
+  8: 'server-boost',
+  9: 'server-boost-tier-1',
+  10: 'server-boost-tier-2',
+  11: 'server-boost-tier-3',
+  12: 'follow',
+  14: 'discovery-disqualified',
+  15: 'discovery-requalified',
+  19: 'reply',
+  20: 'application-command'
+};
+
+const attributes = {
+  'vz-bot-vizality': message => Boolean(message.author?.phone === toHash('VIZALITY') && message.author?.avatar !== 'clyde') && '',
+  'vz-bot-plugin': message => Boolean(message.author?.phone === toHash('PLUGIN') && message.author?.avatar !== 'clyde') && '',
+  'vz-user-id': message => new RegExp(Regexes.USER_ID).test(message.author?.id) && message.author?.id,
+  'vz-local': message => Boolean(message.author?.isLocalBot ? message.author?.isLocalBot() : null) && '',
+  'vz-mentioned': message => Boolean(message.mentioned) && '',
+  'vz-self': message => Boolean(message.author?.email) && '',
+  'vz-webhook': message => Boolean(message.webhookId) && '',
+  'vz-bot': message => Boolean(message.author?.bot) && '',
+  'vz-member': (message, channel, _, memberModule) => Boolean(
+    channel?.guild_id &&
+    memberModule.getMember(channel?.guild_id, message.author.id) &&
+    message.type === 0
+  ) && '',
+  'vz-owner': (message, channel, guildModule) => Boolean(
+    channel?.guild_id &&
+    message.author?.id === guildModule.getGuild(channel?.guild_id) &&
+    message.type === 0
+  ) && '',
+  /*
+   * Message-related attributes.
+   */
+  'vz-attachment': message => Boolean(message.attachments?.length) && '',
+  'vz-embed': message => Boolean(message.embeds?.length) && '',
+  'vz-blocked': message => Boolean(message.blocked) && '',
+  'vz-message-id': message => message.id,
+  /**
+   * Set an attribute for special types of messages.
+   * @see {@link https://discord.com/developers/docs/resources/channel#message-object-message-types}
+   */
+  'vz-type': message => messageTypes[message.type.toString()]
+};
+
 export default main => {
-  const Message = getModule('getElementFromMessageId')?.default;
+  const Message = getModule(m => m && m.default && m.default.toString().search('childrenRepliedMessage') > -1);
   const memberModule = getModule('getMember');
   const guildModule = getModule('getGuild');
-  patch('vz-attributes-messages', Message, 'type', (_, res) => {
+  patch('vz-attributes-messages', Message, 'default', (_, res) => {
     try {
       const props = findInTree(res, n => n?.message && n.channel);
       /*
@@ -18,55 +70,16 @@ export default main => {
        */
       if (!props?.message) return;
       const { message, channel } = props;
-      /*
-       * Author-related attributes.
-       */
-      res.props['vz-bot-vizality'] = Boolean(message.author?.phone === toHash('VIZALITY') && message.author?.avatar !== 'clyde') && '';
-      res.props['vz-bot-plugin'] = Boolean(message.author?.phone === toHash('PLUGIN') && message.author?.avatar !== 'clyde') && '';
-      res.props['vz-user-id'] = new RegExp(Regexes.USER_ID).test(message.author?.id) && message.author?.id;
-      res.props['vz-local'] = Boolean(message.author?.isLocalBot ? message.author?.isLocalBot() : null) && '';
-      res.props['vz-mentioned'] = Boolean(message.mentioned) && '';
-      res.props['vz-self'] = Boolean(message.author?.email) && '';
-      res.props['vz-webhook'] = Boolean(message.webhookId) && '';
-      res.props['vz-bot'] = Boolean(message.author?.bot) && '';
-      res.props['vz-member'] = Boolean(
-        channel?.guild_id &&
-        memberModule.getMember(channel?.guild_id, message.author.id) &&
-        message.type === 0
-      ) && '';
-      res.props['vz-owner'] = Boolean(
-        channel?.guild_id &&
-        message.author?.id === guildModule.getGuild(channel?.guild_id) &&
-        message.type === 0
-      ) && '';
-      /*
-       * Message-related attributes.
-       */
-      res.props['vz-attachment'] = Boolean(message.attachments?.length) && '';
-      res.props['vz-embed'] = Boolean(message.embeds?.length) && '';
-      res.props['vz-blocked'] = Boolean(message.blocked) && '';
-      res.props['vz-message-id'] = message.id;
-      /**
-       * Set an attribute for special types of messages.
-       * @see {@link https://discord.com/developers/docs/resources/channel#message-object-message-types}
-       */
-      res.props['vz-type'] =
-        (Boolean(message.type === 1) && 'recipient-add') ||
-        (Boolean(message.type === 2) && 'recipient-remove') ||
-        (Boolean(message.type === 3) && 'call') ||
-        (Boolean(message.type === 4) && 'channel-name-change') ||
-        (Boolean(message.type === 5) && 'channel-icon-change') ||
-        (Boolean(message.type === 6) && 'pin') ||
-        (Boolean(message.type === 7) && 'member-join') ||
-        (Boolean(message.type === 8) && 'server-boost') ||
-        (Boolean(message.type === 9) && 'server-boost-tier-1') ||
-        (Boolean(message.type === 10) && 'server-boost-tier-2') ||
-        (Boolean(message.type === 11) && 'server-boost-tier-3') ||
-        (Boolean(message.type === 12) && 'follow') ||
-        (Boolean(message.type === 14) && 'discovery-disqualified') ||
-        (Boolean(message.type === 15) && 'discovery-requalified') ||
-        (Boolean(message.type === 19) && 'reply') ||
-        (Boolean(message.type === 20) && 'application-command');
+
+      if (!res.props?.children?.props) throw 'children element was not found!';
+
+      for (const attribute in attributes) {
+        try {
+          res.props.children.props[attribute] = attributes[attribute](message, channel, guildModule, memberModule);
+        } catch (error) {
+          main.error(main._labels.concat(labels.concat('Message')), `Failed to add attribute "${attribute}"!\n`, error);
+        }
+      }
     } catch (err) {
       return main.error(main._labels.concat(labels.concat('Message')), err);
     }
